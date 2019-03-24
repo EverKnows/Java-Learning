bce516dd86aa010d4570bfaeecfda4142c14f4e2
ranly <1193932296@qq.com>
2019-03-24 17:15:08 +0800

    hotel management system

---
+++ HotelManagementSystem/Room.java
@@ -0,0 +1,7 @@
+package com.zzxx.Test;
+
+public class Room {
+    public String roomNum;
+    public String tenantName = null;
+
+}
---
+++ HotelManagementSystem/hotelSystemTest.java
@@ -0,0 +1,10 @@
+package com.zzxx.Test;
+
+
+public class hotelSystemTest {
+    public static void main(String[] args) {
+        HotelManagementSystem hotel = new HotelManagementSystem("zzxx酒店");
+        hotel.mainInterface();
+    }
+
+}
